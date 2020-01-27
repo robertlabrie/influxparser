a1f41ef0b791b4111778fc609832862a7c18fc5d
Robert Labrie <robert.labrie@gmail.com>
2020-01-27 11:12:17 -0500

    +parsing

--- points.rb
+++ points.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 def parse_point(s)
     point = {}
     point['_raw'] = s
@@ -7,13 +8,13 @@ def parse_point(s)
     return false if parts.length < 2 # at bare minimum there needs to be a measurement and some fields
 
     mparts = parts[0].split(/(?<!\\),/) # split on unescaped commas for the measurement name and tags
-    point['measurement'] = mparts[0]
+    point['measurement'] = unescape_measurement mparts[0]
     
     # if any tags were attached to the measurement iterate over them now
     point['tags'] = {}
     mparts.drop(1).each do |t|
         tag = t.split(/(?<!\\)=/)
-        point['tags'][tag[0]] = tag[1]
+        point['tags'][unescape_tag tag[0]] = unescape_tag tag[1]
     end
     
     # now iterate over the values
@@ -21,9 +22,34 @@ def parse_point(s)
     vparts = parts[1].split(/(?<!\\),/)
     vparts.each do |v|
         value = v.split(/(?<!\\)=/)
-        point['values'][value[0]] = value[1]
+        point['values'][unescape_tag value[0]] = unescape_point value[1]
+    end
+
+    if parts.length == 3 # handle the timestamp
+        point['time'] = parts[2]
+    else
+        # no time left for you, on my way to better things
+        point['time'] = nil
     end
     point
 end
-string = 'wea\ ther,location=us-midwest temperature=82 1465839830100400200'
-puts parse_point string
+def unescape_measurement(s)
+    s.gsub(/\\ /,' ').gsub(/\\,/,',')
+end
+def unescape_tag(s)
+    t = unescape_measurement s
+    t.gsub(/\\=/,'=')
+end
+def unescape_point(s)
+    if s[-1,1] == '"'
+
+        # it's a quoted string and should be unescaped
+        s = s[1..-2] # take the wrapping quotes off
+        return s.gsub(/\\\\/,',').gsub(/\\"/,',')
+    else
+        # it's a number and if it's trailing an i we need to strip it because we're not handling precision here
+        return s.chomp('i')
+    end
+end
+string = 'wea\ ther,location=us-midwest temper\=ature=82i 1465839830100400200'
+puts parse_point(string).to_yaml
